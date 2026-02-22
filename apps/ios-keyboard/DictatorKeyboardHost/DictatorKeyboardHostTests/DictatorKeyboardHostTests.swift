69ff3ff23e07ec4986248dd168b31e5c151b3516
joyo <joyofresh@gmail.com>
2026-02-21 19:19:08 -0800

    Add Xcode iOS host+keyboard project setup and target wiring files

---
+++ apps/ios-keyboard/DictatorKeyboardHost/DictatorKeyboardHostTests/DictatorKeyboardHostTests.swift
@@ -0,0 +1,17 @@
+//
+//  DictatorKeyboardHostTests.swift
+//  DictatorKeyboardHostTests
+//
+//  Created by Joyo on 2/21/26.
+//
+
+import Testing
+@testable import DictatorKeyboardHost
+
+struct DictatorKeyboardHostTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
